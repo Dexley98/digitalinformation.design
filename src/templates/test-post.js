ac61c2db146216c2afefd1e317583782be25ff91
Dom Exley <dominique.exley@gmail.com>
2020-03-23 16:51:49 -0400

    fixed merge conflicts. backend and origin are synced 03/23/2020

--- src/templates/test-post.js
+++ src/templates/test-post.js
@@ -25,44 +25,6 @@ class Test extends Component {
          console.log(splashMedia)
 
          return(
-<<<<<<< HEAD
-            <Layout>
-                <div>
-                    <section className="splashMedia-block">
-                        <img src={splashMedia[0].fluid.src} alt="splash media" />
-                        <h2>{tagline}</h2>
-                        <h1>{taglineList[2]}</h1>
-                    </section>
-                    <section className="concentrationSummary-block">
-                        <h2>{slug}</h2>
-                        <p>{concentrationSummary}</p>
-                    </section>
-                    <section className="whatDoesItMean-block">
-                        <img src={concentrationAsset.fluid.src} alt={concentrationAsset.description} />
-                        <h2>WHAT DOES IT MEAN?</h2>
-                        <p>{WhatDoesItMean.WhatDoesItMean}</p>
-                    </section>
-                    <section className="career-block">
-                        <h2>CAREERS</h2>
-                        <p>THIS IS GOING TO BE WHERE THE CAREER COMPONENTS GO.</p>
-                        <p>Do you want to learn more about the careers that our program can prepare you for?</p>
-                        <p>LINK WILL GO HERE.</p>
-                    </section>
-                    <section className="graduate-block">
-                        <h2>HEAR FROM OUR GRADUATES</h2>
-                        <p>LOREM IPSUM IS HERE CURRENTLY. DESCRIBE THE CAREERS YO.</p>
-                        <p>THIS IS GOING TO BE WHERE THE GRADUATES COMPONENTS WILL GO.</p>
-                    </section>
-                    <section className="learningOutcomes-block">
-                        <h2>KEY LEARNING OUTCOMES</h2>
-                        <p>{learningOutcomesSummary.learningOutcomesSummary}</p>
-                        <img src={learningOutcomeAsset.fluid.src} alt={learningOutcomeAsset.description} />
-                        <p>LEARNING OUTCOMES COMPONENTS WILL GO HERE.</p>
-                    </section>
-                    
-                </div>
-             </Layout>
-=======
              <div>
                 <section className="splashMedia-block">
                     <img src={splashMedia[0].fixed.src} alt="splash media" />
@@ -97,7 +59,6 @@ class Test extends Component {
                 </section>
                  
              </div>
->>>>>>> origin
          )
      }
 }
