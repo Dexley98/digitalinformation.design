f02e29c78de8af21ddb5985882c6f3cbd6f0e622
Dom Exley <dominqiue.exley@gmail.com>
2020-03-27 11:54:44 -0400

    templating for both page sets is done

--- src/templates/parentPage.js
+++ src/templates/parentPage.js
@@ -6,13 +6,60 @@ import Layout from "../components/layout"
 export default class parentPage extends Component {
     render() {
         const {
-            slug
+            concentrationTitle,
+            mainActivity,
+            splashMedia,
+            concentrationSummary,
+            employmentOutlook,
+            jobOpportunities,
+            concentrationAsset,
+            medianSalary
          } = this.props.data.contentfulConcentrationPageParents;
 
         return (
             <Layout>
                 <div>
-                    <h1>Welcome to the {slug} page!</h1>
+                <section className="splashMedia-block">
+                    <img src={splashMedia[0].fixed.src} alt={splashMedia[0].description} />
+                    <h1>{concentrationTitle}</h1>
+                    <h2>{mainActivity}</h2>
+                </section>
+                <section className="summary-block">
+                    <h3>What is {concentrationTitle}</h3>
+                    <p>{concentrationSummary.concentrationSummary}</p>
+                </section>
+                <section className="employment-outlook-block">
+                    <p>{employmentOutlook.employmentOutlook}</p>
+                </section>
+                <section className="job-oppurtunities-block">
+                    <h3>Job Opportunities</h3>
+                    <p>{jobOpportunities.jobOpportunities}</p>
+                    <img src={concentrationAsset.fixed.src} alt={concentrationAsset.description} />
+                </section>
+                <section className="career-block">
+                    <h2>CAREERS</h2>
+                    <p>THIS IS GOING TO BE WHERE THE CAREER COMPONENTS GO.</p>
+                    <p>Do you want to learn more about the careers that our program can prepare you for?</p>
+                    <p>LINK WILL GO HERE.</p>
+                </section>
+                <section className="median-salary-block">
+                    <p>{medianSalary.medianSalary}</p>
+                </section>
+                <section className="graduate-block">
+                    <h2>HEAR FROM OUR GRADUATES</h2>
+                    <p>LOREM IPSUM IS HERE CURRENTLY. DESCRIBE THE CAREERS YO.</p>
+                    <p>THIS IS GOING TO BE WHERE THE GRADUATES COMPONENTS WILL GO.</p>
+                </section>
+                <section className="student-work-block">
+                  <h3>STUDENT WORK</h3>
+                  <p>Our students are always hard at work in their classes. Here are some finished projects that demonstrate what you can learn to do.</p>
+                  <p>STUDENT WORK COMPONENTS WILL GO HERE.</p>
+                </section>
+                <section className="apply-now-block">
+                  <h3>APPLY NOW</h3>
+                  <p>Interested? Put your future on the right track today!</p>
+                  <p>APPLY NOW LINK WILL GO HERE.</p>
+                </section>
                 </div>
             </Layout>
         )
@@ -26,6 +73,33 @@ parentPage.propTypes = {
 export const pageQuery = graphql`
 query concentrationPageParentsQuery($slug: String!){
     contentfulConcentrationPageParents(slug: {eq: $slug}) {
+        id
         slug
+        concentrationTitle
+        mainActivity
+        splashMedia {
+            description
+            fixed {
+                src
+            }
+        }
+        concentrationSummary {
+            concentrationSummary
+        }
+        employmentOutlook {
+            employmentOutlook
+        }
+        jobOpportunities {
+            jobOpportunities
+        }
+        concentrationAsset {
+            description
+            fixed {
+                src
+            }
+        }
+        medianSalary {
+            medianSalary
+        }
     }
 }`
--- src/templates/studentPage.js
+++ src/templates/studentPage.js
@@ -8,7 +8,6 @@ class studentPage extends Component {
      render(){
       const {
         slug,
-        tagline,
         taglineList,
         splashMedia,
         concentrationSummary,
@@ -30,7 +29,6 @@ class studentPage extends Component {
              <div>
                 <section className="splashMedia-block">
                     <img src={splashMedia[0].fixed.src} alt="splash media" />
-                    <h2>{tagline}</h2>
                     <div className="word-container">
                         {taglineList.map((item) =>{
                           console.log('WHAT IS INSIDE TAGLINE LIST', item);
@@ -39,32 +37,45 @@ class studentPage extends Component {
                     </div>
                 </section>
                 <section className="concentrationSummary-block">
-                    <h2>{slug}</h2>
+                    <h3>{slug}</h3>
                     <p>{concentrationSummary}</p>
                 </section>
                 <section className="whatDoesItMean-block">
                     <img src={concentrationAsset.fixed.src} alt={concentrationAsset.description} />
-                    <h2>WHAT DOES IT MEAN?</h2>
+                    <h3>WHAT DOES IT MEAN?</h3>
                     <p>{WhatDoesItMean.WhatDoesItMean}</p>
                 </section>
                 <section className="career-block">
-                    <h2>CAREERS</h2>
+                    <h3>CAREERS</h3>
                     <p>THIS IS GOING TO BE WHERE THE CAREER COMPONENTS GO.</p>
                     <p>Do you want to learn more about the careers that our program can prepare you for?</p>
                     <p>LINK WILL GO HERE.</p>
                 </section>
                 <section className="graduate-block">
-                    <h2>HEAR FROM OUR GRADUATES</h2>
+                    <h3>HEAR FROM OUR GRADUATES</h3>
                     <p>LOREM IPSUM IS HERE CURRENTLY. DESCRIBE THE CAREERS YO.</p>
                     <p>THIS IS GOING TO BE WHERE THE GRADUATES COMPONENTS WILL GO.</p>
                 </section>
                 <section className="learningOutcomes-block">
-                    <h2>KEY LEARNING OUTCOMES</h2>
+                    <h3>KEY LEARNING OUTCOMES</h3>
                     <p>{learningOutcomesSummary.learningOutcomesSummary}</p>
                     <img src={learningOutcomeAsset.fixed.src} alt={learningOutcomeAsset.description} />
                     <p>LEARNING OUTCOMES COMPONENTS WILL GO HERE.</p>
                 </section>
-                 
+                <section className="student-work-block">
+                  <h3>STUDENT WORK</h3>
+                  <p>Our students are always hard at work in their classes. Here are some finished projects that demonstrate what you can learn to do.</p>
+                  <p>STUDENT WORK COMPONENTS WILL GO HERE.</p>
+                </section>
+                <section className="coursework-block">
+                  <p>Here's a taste of the classes you may take while in DIFD.</p>
+                  <p>BIG ASS BLOCK OF COURSES WILL GO HERE.</p>
+                </section>
+                <section className="apply-now-block">
+                  <h3>APPLY NOW</h3>
+                  <p>Interested? Put your future on the right track today!</p>
+                  <p>APPLY NOW LINK WILL GO HERE.</p>
+                </section>
              </div>
          )
      }
@@ -79,7 +90,6 @@ export default studentPage
 export const pageQuery = graphql`
 query concentrationPageHomeQuery($slug: String!){
     contentfulConcentrationPageHome(slug: {eq: $slug}) {
-      tagline
       taglineList
       splashMedia {
         fixed {
