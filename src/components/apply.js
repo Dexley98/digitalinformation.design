8bd98a72ef18c2f314b069b0c743ad3a4f2e5bd2
Alissa Melsopp <melsoppa2@mailbox.winthrop.edu>
2020-04-04 13:48:21 -0400

    design changes for student pages

--- src/components/apply.js
+++ src/components/apply.js
@@ -4,10 +4,13 @@ export default class Apply extends Component {
     render() {
         return (
             <section className="apply-now-block">
+              <div></div>
+              <div>
                 <h2>APPLY NOW</h2>
                 <p>Interested? Put your future on the right track today!</p>
                 {/*External links must still use the a tag accoding to gatsby link docs. */}
                 <a href="https://apply.winthrop.edu/apply/">Apply Now!</a>
+              </div>
             </section>
         )
     }
