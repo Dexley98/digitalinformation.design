aaa6c3b3b51a9d2dfb8d08c0e9a7a0b81b29a7a3
Dom Exley <dominqiue.exley@gmail.com>
2020-04-09 13:34:36 -0400

    made grad block conditional render

--- src/templates/studentPage.js
+++ src/templates/studentPage.js
@@ -121,25 +121,27 @@ class studentPage extends Component {
                 </section>
 
 {/* Graduate Section */}
-                <section className="graduate-block">
-                  <div className="top-curve"></div>
-                  <div className="middle-bg">
-                    <h2>HEAR FROM OUR GRADUATES</h2>
-                    <p>See where past members of our program are today.</p>
-                    <div className="grad-blob-container">
-                      {gradList.map((index) =>{
-                        return(
-                        <Grad
-                          imgSrc={index.node.picture.file.url}
-                          gradName={index.node.name}
-                          jobTitle={index.node.jobTitle}
-                          gradBio={index.node.bio.bio}
-                        />)
-                      })}
+              { gradList.length > 0 &&
+                  <section className="graduate-block">
+                    <div className="top-curve"></div>
+                    <div className="middle-bg">
+                      <h2>HEAR FROM OUR GRADUATES</h2>
+                      <p>See where past members of our program are today.</p>
+                      <div className="grad-blob-container">
+                        {gradList.map((index) =>{
+                          return(
+                          <Grad
+                            imgSrc={index.node.picture.file.url}
+                            gradName={index.node.name}
+                            jobTitle={index.node.jobTitle}
+                            gradBio={index.node.bio.bio}
+                          />)
+                        })}
+                      </div>
                     </div>
-                  </div>
-                  <div className="bottom-curve"></div>
-                </section>
+                    <div className="bottom-curve"></div>
+                  </section>
+              }
 
 {/* Learning Outcome Section */}
                 <section className="learningOutcomes-block">
