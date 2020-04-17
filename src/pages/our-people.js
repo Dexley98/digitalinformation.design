133e1d70c9d4798eff647a8bb9c562403be47133
Dom Exley <dominique.exley@gmail.com>
2020-04-17 09:12:32 -0400

    should be the last time I have to change this our-people page

--- src/pages/our-people.js
+++ src/pages/our-people.js
@@ -131,6 +131,7 @@ export const pageQuery = graphql`
                     url
                   }
                 }
+                jobTitle
                 gradName
                 gradDate {
                   gradDate
