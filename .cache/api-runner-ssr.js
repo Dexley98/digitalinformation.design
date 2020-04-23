1735ccb6592cac46f4c1e6198bd8b2fa5cbcbc5c
Alissa Melsopp <melsoppa2@mailbox.winthrop.edu>
2020-04-23 17:29:14 -0400

    Fixed logo issues and removed site max-width

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,6 +1,12 @@
 var plugins = [{
       plugin: require('/Users/thesqueeze/Desktop/DIFD451/digitalinformation.design/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"digitalinformation.design","short_name":"DIF.D","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/DIFD-logo.png"},
+    },{
+      plugin: require('/Users/thesqueeze/Desktop/DIFD451/digitalinformation.design/node_modules/gatsby-plugin-recaptcha/gatsby-ssr'),
+      options: {"plugins":[],"async":false,"defer":false,"args":"?onload=onloadCallback&render=explict"},
+    },{
+      plugin: require('/Users/thesqueeze/Desktop/DIFD451/digitalinformation.design/node_modules/gatsby-plugin-gtag/gatsby-ssr'),
+      options: {"plugins":[],"trackingId":"UA-159214106-1","head":true,"anonymize":false},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
