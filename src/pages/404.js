679c4a7d29d8512342cb7167cc6eaa6172daddbf
Dom Exley <dominqiue.exley@gmail.com>
2020-04-23 19:43:50 -0400

    added to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,63 @@
-import React from "react"
+import React, {Component} from "react"
+import {Link} from 'gatsby'
+
+import MainMenu from '../components/main-menu'
+import Footer from '../components/footer'
+import SideDrawer from '../components/side-drawer'
+import BackDrop from '../components/back-drop'
 
 import "../css/layout.css"
 
-const NotFoundPage = () => (
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-)
+class NotFoundPage extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        sideDrawerOpen: false
+      }
+    }
+
+    drawerToggleClickHandler = () =>{
+      this.setState((prevState) => {
+        return {sideDrawerOpen: !prevState.sideDrawerOpen}
+      })
+    }
+
+    backdropClickHandler = () => {
+      this.setState({sideDrawerOpen: false})
+    }
+
+    render(){
+      
+      let sideDrawer = null;
+      let backDrop = null;
+      if (this.state.sideDrawerOpen) {
+        sideDrawer = <SideDrawer />
+        backDrop = <BackDrop click={this.backdropClickHandler}/>
+      }
+
+      return(
+        <div>
+          <MainMenu drawerClickHandler={this.drawerToggleClickHandler}/>
+          {sideDrawer}
+          {backDrop}
+
+          <h1>Fiddle Sticks!</h1>
+          <h3>Are you lost?</h3>
+          <p>You're looking for something that doesn't exist...</p>
+          <br/>
+          <p>Try going to our homepage.</p>
+          <p><Link to='/#top'
+              state={{ prevPath: typeof window !== 'undefined' ?  window.location.pathname  :  ' '}}
+              className="main-link">
+                GO
+          </Link></p>
+          <Footer />
+        </div>
+      )
+    }
+    
+    
+}
 
 
 export default NotFoundPage
